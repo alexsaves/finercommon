e827cb1ae404ee4d4a48ba3606b039fc62f94915
Alexei White <white@Alexeis-MacBook-Pro.local>
2017-12-05 22:42:52 -0800

    Stubbed out BuyX module

---
+++ models/buyx.js
@@ -0,0 +1,35 @@
+/**
+ * All the weightings for BuyX
+ */
+var BuyXWeightings = [
+  {
+    qname: "overall",
+    weight: 0.9
+  },
+  {
+    qname: "overall",
+    weight: 0.9
+  },
+  {
+    qname: "overall",
+    weight: 0.9
+  },
+  {
+    qname: "overall",
+    weight: 0.9
+  },
+  {
+    qname: "overall",
+    weight: 0.9
+  },
+  {
+    qname: "overall",
+    weight: 0.9
+  },
+];
+
+module.exports = {
+  CalculateBuyXFromResponses: function(responses) {
+
+  }
+};
