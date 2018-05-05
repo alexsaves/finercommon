a49a772f0d6bb98ec9570cc00b381d285e7f2de3
Alexei White <white@Alexeis-MacBook-Pro.local>
2018-05-05 15:42:10 -0700

    Fixed dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 const gulp = require('gulp');
 const fs = require('fs');
-const mjml = require('mjml');
 
 var src = {
     email: './fixtures/emails/src/'
@@ -8,30 +7,6 @@ var src = {
   dest = {
     email: './fixtures/emails/dist/'
   };
-
-// Rich html emails
-gulp.task('mjml', function (cb) {
-  var fls = fs.readdirSync(src.email),
-    basetemplate = fs
-      .readFileSync(src.email + "_base.mjml")
-      .toString();
-  for (var i = 0; i < fls.length; i++) {
-    let file = fls[i];
-    if (file.indexOf("_base") == -1 && file.indexOf(".mjml") > -1) {
-      let fcontents = fs
-        .readFileSync(src.email + file)
-        .toString();
-      var finalTemplate = basetemplate.replace(/\<\%\= main \%\>/, fcontents);
-      //console.log(finalTemplate);
-      fcontents = mjml.mjml2html(finalTemplate);
-      
-      if (fcontents.errors.length > 0) {
-        console.log("Errors", fcontents.errors);
-      }
-      fs.writeFileSync(dest.email + file.replace(".mjml", ".html"), fcontents.html);
-    }
-  }
-});
 
 // Text-only emails
 gulp.task('rawemails', function (cb) {
@@ -52,4 +27,4 @@ gulp.task('rawemails', function (cb) {
   }
 });
 
-gulp.task('default', ['mjml', 'rawemails']);
+gulp.task('default', ['rawemails']);
