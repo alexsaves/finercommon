385fefd709bfb8f3cde920a22c4db9f635a56256
Alexei White <white@Alexeis-MacBook-Pro.local>
2018-05-05 20:01:20 -0700

    babel

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
  * Set up an object to expose
  */
 var FinerCommon = {
-  models: require('./models/all')
+  models: require('./models/all'),
+  ChartRenderer: require('./renderer'),
+  MockData: require('./mock-data.json')
 };
 
 // Expose it
