232bb0c5ded99c91cdcb373675bf073947d1e252
Alexei White <white@Alexeis-MacBook-Pro.local>
2018-05-27 22:14:20 -0700

    ratings bar

--- models/charts.js
+++ models/charts.js
@@ -1,5 +1,10 @@
 const streamToBuffer = require('stream-to-buffer');
 
+const fontFace = "Arial";
+const lightColor = "#f3f3f3";
+const darkerColorLight = "#424242";
+const darkerColor = "#757575";
+
 /**
  * Charting class
  */
@@ -63,6 +68,128 @@ class Charts {
     });
   }
 
+
+  /**
+   * Draw an individual score strip
+   * @param {Canvas} ctx 
+   * @param {Number} x 
+   * @param {Number} y 
+   * @param {Number} w 
+   * @param {Number} generalScale 
+   * @param {Number} fontSize 
+   * @param {Number} barHeight 
+   * @param {Number} fontHeight 
+   * @param {Object} rowData 
+   * @param {Number} maxScore 
+   * @param {Number} barColor 
+   */
+  _drawScoreStrip(ctx, x, y, w, generalScale, fontSize, barHeight, fontHeight, rowData, maxScore, barColor) {
+    ctx.font = fontSize + "px " + fontFace;
+    ctx.fillStyle = darkerColorLight;
+    ctx.strokeStyle = darkerColorLight;
+    ctx.textAlign = "left";
+    ctx.fillText(rowData.label, x, y + fontHeight - (fontHeight * 0.3), (w / 2));
+    const barZoneWidth = w / 2;
+    const padding = barZoneWidth / 40;
+    const barWidth = (barZoneWidth - ((maxScore - 1) * padding)) / maxScore;
+    let leftPosition = w - barZoneWidth;
+    const barYOffset = (fontHeight - barHeight) / 2.5;
+    for (let i = 0; i < maxScore; i++) {
+      ctx.fillStyle = lightColor;
+      if (i+1 <= rowData.score) {
+        ctx.fillStyle = barColor;
+      }
+      ctx.fillRect(leftPosition + x, y + barYOffset, barWidth, barHeight);
+      leftPosition += barWidth + padding;
+    }
+  }
+
+  /**
+   * Draw a score strip\
+   * Sample data:
+   * [
+      [{
+          label: "Price",
+          score: 3
+        },
+        {
+          label: "Reports",
+          score: 2
+        },
+        {
+          label: "Service",
+          score: 4
+        }
+      ],
+      [{
+          label: "Product",
+          score: 5
+        },
+        {
+          label: "Timeliness",
+          score: 1
+        },
+        {
+          label: "Integration",
+          score: 0
+        }
+      ],
+      [{
+          label: "Service",
+          score: 3
+        },
+        {
+          label: "Product",
+          score: 5
+        },
+        {
+          label: "Timeliness",
+          score: 2
+        }
+      ]
+    ]
+   * @param {Number} w 
+   * @param {Number} maxScore 
+   * @param {Array} data 
+   * @param {Number} paddingOverride 
+   * @param {Number} fontSizeOverride 
+   */
+  async scoreStripsAsync(w, maxScore, data, paddingOverride, fontSizeOverride) {
+    const cols = data.length;
+    const generalScale = w / 500;
+    const padding = paddingOverride || (generalScale * 24);
+    const fontSize = Math.round(fontSizeOverride || (generalScale * 11));
+    const fontHeight = Math.round(fontSize * 1.3);
+    const colSize = ((w - ((cols - 1) * padding)) / cols);
+    const barHeight = fontHeight * 0.3;
+    const barColors = ["#3845ae", "#ffc200", "#8fa4af"];
+    const maxRows = data.reduce((previousValue, col) => {
+      return Math.max(previousValue, col.length);
+    }, 0);
+    const h = maxRows * fontHeight;
+    let leftPosition = 0;
+
+    if (arguments.length < 3) {
+      throw new Error("Missing arguments");
+    }
+
+    const Canvas = require('canvas');
+    var Image = Canvas.Image,
+      canvas = new Canvas(w, h),
+      ctx = canvas.getContext('2d');
+
+    for (let c = 0; c < cols; c++) {
+      let topPosition = 0;
+      for (let r = 0; r < data[c].length; r++) {
+        let row = data[c][r];
+        this._drawScoreStrip(ctx, leftPosition, topPosition, colSize, generalScale, fontSize, barHeight, fontHeight, row, maxScore, barColors[c]);
+        topPosition += fontHeight;
+      }
+      leftPosition += colSize + padding;
+    }
+    return await this._canvasToPNGBufferAsync(canvas);
+  }
+
   /**
    * Draw a bar chart widget.
    * Example data object:
@@ -92,7 +219,7 @@ class Charts {
    * @param {Number} fontSizeOverride 
    * @param {Number} borderRadiusOverride 
    */
-  async barChartAsync(w, h, data, paddingOverride, fontSizeOverride, borderRadiusOverride) {    
+  async barChartAsync(w, h, data, paddingOverride, fontSizeOverride, borderRadiusOverride) {
     const Canvas = require('canvas');
     var Image = Canvas.Image,
       canvas = new Canvas(w, h),
@@ -111,11 +238,7 @@ class Charts {
     const colSize = ((w - ((cols - 1) * padding)) / cols);
     const cornerRadius = borderRadiusOverride || (generalScale * 4);
     const barHeight = h - (padding * 4);
-    const spaceFromBottom = generalScale * 3;
-    const fontFace = "Arial";
-    const lightColor = "#f3f3f3";
-    const darkerColorLight = "#424242";
-    const darkerColor = "#757575";
+    const spaceFromBottom = generalScale * 3;    
     const spacerSize = {
       w: Math.max(2, (generalScale * 3)),
       h: (padding * 0.6)
